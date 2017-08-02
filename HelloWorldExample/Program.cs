5f689c60ef980bd296c19bf0acf94034e06ef8b6
Alexey Kornienko <kornienko1296@gmail.com>
2017-08-02 15:07:55 +0300

    Add project files.

---
+++ HelloWorldExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("helloworld 1");
+        }
+    }
+}
