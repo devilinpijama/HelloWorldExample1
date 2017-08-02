fee452db0fddef0edf5bd44c1e7c9da0473dd805
Alexey Kornienko <kornienko1296@gmail.com>
2017-08-02 15:34:55 +0300

    Release2

--- HelloWorldExample/Program.cs
+++ HelloWorldExample/Program.cs
@@ -14,7 +14,8 @@ namespace HelloWorldExample
         }
         static void Main(string[] args)
         {
-            
+            for (int i=0;i<3;i++)
+                Console.WriteLine(i);
 
         }
     }
